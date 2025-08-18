016bdd92d68c38ee8527b09d6b1bdb25bc7af57a
taimoorsarwar-dev <taimoorsarwar.dev@gmail.com>
2025-08-18 16:19:11 +0400

    clean and optimized add to cart button

--- lib/modules/cart/add_to_cart_button.dart
+++ lib/modules/cart/add_to_cart_button.dart
@@ -28,12 +28,10 @@ class AddToCartButton extends StatelessWidget {
       builder: (context, snapshot) {
         int currentQuantity = 0;
         CartModel? cart = snapshot.data;
-
-        double btnSize = UIHelper.outlineButtonHeight;
-
-        if (showCompatButton) {
-          btnSize = UIHelper.chipButtonHeight;
-        }
+        double btnSize =
+            showCompatButton
+                ? UIHelper.chipButtonHeight
+                : UIHelper.outlineButtonHeight;
 
         // Get current quantity from cart
         if (cart != null) {
@@ -49,129 +47,155 @@ class AddToCartButton extends StatelessWidget {
         }
 
         if (currentQuantity == 0) {
-          if (showCompatButton) {
-            return CustomIconButton(
-              icon: R.assetsImagesIconsAdd,
-              buttonIconSize: ButtonIconSize.extraExtraExtraSmall,
-              fillColor: ThemePalette.selectedTextColor,
-              iconColor: ThemePalette.accentColor,
-              borderColor: ThemePalette.accentColor,
-              showBorder: true,
-              isCircular: true,
-              onTap: () {
-                cartBloc?.manageCart(productModel: model, quantity: 1);
-              },
-              size: btnSize,
-            );
-          }
-          return PrimaryButton(
-            title: AppLocalizations.getLocalization().addToCart,
-            onTap: () {
-              cartBloc?.manageCart(productModel: model, quantity: 1);
-            },
-            padding: EdgeInsets.symmetric(horizontal: UIHelper.smallPadding),
-            textStyle: AppTextStyle.getMediumTextStyle(
-              false,
-              ThemePalette.selectedTextColor,
-              FontType.bold,
-            ),
-            height: btnSize,
-          );
+          return _getAddToCartButtonWidget(btnSize: btnSize);
         }
-
-        if (showCompatButton) {}
-
-        return Container(
-          height: btnSize,
-          decoration: BoxDecoration(
-            color: ThemePalette.accentColor,
-            borderRadius: UIHelper.getBorderRadius(
-              radius: UIHelper.smallRadius,
-            ),
-          ),
-          child: ClipRRect(
-            borderRadius: UIHelper.getBorderRadius(
-              radius: UIHelper.smallRadius,
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                MaterialButton(
-                  onPressed: () {
-                    cartBloc?.manageCart(
-                      productModel: model,
-                      quantity: currentQuantity - 1,
-                    );
-                  },
-                  minWidth: 0,
-                  padding: EdgeInsets.zero,
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  child: SizedBox(
-                    width: btnSize,
-                    child: Center(
-                      child: Text(
-                        "-",
-                        style: AppTextStyle.getExtraLargeTextStyle(
-                          false,
-                          ThemePalette.selectedTextColor,
-                          FontType.medium,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width:
-                      showCompatButton
-                          ? UIHelper.tagItemHeight
-                          : UIHelper.chipButtonHeight,
-                  child: Center(
-                    child: Text(
-                      currentQuantity.toString(),
-                      style: AppTextStyle.getLargeTextStyle(
-                        false,
-                        ThemePalette.selectedTextColor,
-                        FontType.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    if (currentQuantity >= 3) {
-                      CustomToast.show(
-                        "Max quantity added",
-                        type: ToastType.generic,
-                      );
-                    } else {
-                      cartBloc?.manageCart(
-                        productModel: model,
-                        quantity: currentQuantity + 1,
-                      );
-                    }
-                  },
-                  minWidth: 0,
-                  padding: EdgeInsets.zero,
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  child: SizedBox(
-                    width: btnSize,
-                    child: Center(
-                      child: Text(
-                        "+",
-                        style: AppTextStyle.getExtraLargeTextStyle(
-                          false,
-                          ThemePalette.selectedTextColor,
-                          FontType.medium,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        return _getQuantityWidget(
+          currentQuantity: currentQuantity,
+          btnSize: btnSize,
         );
       },
+    );
+  }
+
+  Widget _getAddToCartButtonWidget({required double btnSize}) {
+    if (showCompatButton) {
+      return CustomIconButton(
+        icon: R.assetsImagesIconsAdd,
+        buttonIconSize: ButtonIconSize.extraExtraExtraSmall,
+        fillColor: ThemePalette.selectedTextColor,
+        iconColor: ThemePalette.accentColor,
+        borderColor: ThemePalette.accentColor,
+        showBorder: true,
+        isCircular: true,
+        onTap: () {
+          cartBloc?.manageCart(productModel: model, quantity: 1);
+        },
+        size: btnSize,
+      );
+    }
+    return PrimaryButton(
+      title: AppLocalizations.getLocalization().addToCart,
+      onTap: () {
+        cartBloc?.manageCart(productModel: model, quantity: 1);
+      },
+      padding: EdgeInsets.symmetric(horizontal: UIHelper.smallPadding),
+      textStyle: AppTextStyle.getMediumTextStyle(
+        false,
+        ThemePalette.selectedTextColor,
+        FontType.bold,
+      ),
+      height: btnSize,
+    );
+  }
+
+  Widget _getQuantityWidget({int currentQuantity = 0, double? btnSize}) {
+    BoxDecoration boxDecoration = BoxDecoration(
+      color: ThemePalette.accentColor,
+      borderRadius: UIHelper.getBorderRadius(radius: UIHelper.smallRadius),
+    );
+    Color textColor = ThemePalette.selectedTextColor;
+
+    if (showCompatButton) {
+      boxDecoration = BoxDecoration(
+        color: ThemePalette.cellBackgroundColor,
+        borderRadius: UIHelper.getBorderRadius(radius: UIHelper.smallRadius),
+        border: UIHelper.getBorder(color: ThemePalette.accentColor),
+      );
+
+      textColor = ThemePalette.primaryText;
+    }
+
+    return Container(
+      height: btnSize,
+      decoration: boxDecoration,
+      child: ClipRRect(
+        borderRadius: UIHelper.getBorderRadius(radius: UIHelper.smallRadius),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _getQuantityButton(
+              currentQuantity: currentQuantity,
+              btnSize: btnSize,
+              btnText: "-",
+              textColor: textColor,
+              onTap: () {
+                cartBloc?.manageCart(
+                  productModel: model,
+                  quantity: currentQuantity - 1,
+                );
+              },
+            ),
+            _getQuantityTextWidget(
+              currentQuantity: currentQuantity,
+              textColor: textColor,
+            ),
+            _getQuantityButton(
+              currentQuantity: currentQuantity,
+              btnSize: btnSize,
+              textColor: textColor,
+              btnText: "+",
+              onTap: () {
+                if (currentQuantity >= 3) {
+                  CustomToast.show(
+                    AppLocalizations.getLocalization().maxQuantityAdded,
+                    type: ToastType.generic,
+                  );
+                } else {
+                  cartBloc?.manageCart(
+                    productModel: model,
+                    quantity: currentQuantity + 1,
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getQuantityTextWidget({int currentQuantity = 0, Color? textColor}) {
+    return SizedBox(
+      width:
+          showCompatButton ? UIHelper.tagItemHeight : UIHelper.chipButtonHeight,
+      child: Center(
+        child: Text(
+          currentQuantity.toString(),
+          style: AppTextStyle.getLargeTextStyle(
+            false,
+            textColor ?? ThemePalette.primaryText,
+            FontType.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getQuantityButton({
+    int currentQuantity = 0,
+    double? btnSize,
+    String? btnText,
+    Color? textColor,
+    VoidCallback? onTap,
+  }) {
+    return MaterialButton(
+      onPressed: onTap,
+      minWidth: 0,
+      padding: EdgeInsets.zero,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      child: SizedBox(
+        width: btnSize,
+        child: Center(
+          child: Text(
+            btnText ?? "",
+            style: AppTextStyle.getExtraLargeTextStyle(
+              false,
+              textColor ?? ThemePalette.primaryText,
+              FontType.medium,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
